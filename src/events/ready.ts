839e9dc1b1fb004833cdb9149baf4e33b8d74d68
Evan <evanrayweigel@outlook.com>
2022-05-28 11:46:35 -0500

    add basic client stuff

---
+++ src/events/ready.ts
@@ -0,0 +1,8 @@
+import { Keymash } from "../types";
+
+export default {
+  event: "ready",
+  run: async (client) => {
+    console.log(`Logged in as ${client.user.tag}`.blue);
+  }
+} as Keymash.Event<"ready">;
---
+++ src/types/index.d.ts
@@ -0,0 +1,39 @@
+import * as Discord from "discord.js";
+import { Client } from "../structures/client";
+
+declare namespace Keymash {
+  interface WPMRole {
+    id: Discord.Snowflake;
+    min: number;
+    max: number;
+  }
+
+  interface ClientOptions extends Discord.ClientOptions {
+    guildID: Discord.Snowflake;
+    devID: Discord.Snowflake;
+    dev: boolean;
+    wpmRoles: WPMRole[];
+  }
+
+  interface Command<T extends Discord.ApplicationCommandType = "CHAT_INPUT"> {
+    name: string;
+    description?: string;
+    category: string;
+    type?: T;
+    options?: Discord.ApplicationCommandOption[];
+    needsPermissions?: boolean;
+    run: (
+      interaction: T extends "CHAT_INPUT"
+        ? Discord.CommandInteraction
+        : T extends "MESSAGE"
+        ? Discord.MessageContextMenuInteraction
+        : Discord.UserContextMenuInteraction,
+      client: Client<true>
+    ) => void;
+  }
+
+  interface Event<E extends keyof Discord.ClientEvents> {
+    event: E;
+    run: (client: Client<true>, ...eventArgs: Discord.ClientEvents[E]) => void;
+  }
+}
