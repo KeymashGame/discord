b43893f4b9622f6f5740c7ca031d98b2aae5d27e
Evan <evanrayweigel@outlook.com>
2022-05-28 17:42:09 -0500

    remove more old code

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    apps: [
-      {
-        name: 'keymash-discord',
-        script: 'yarn',
-        args: 'start',
-        env: {
-            NODE_ENV: "development",
-            DISCORD_TOKEN: process.env.DISCORD_TOKEN
-        },
-        env_production: {
-            NODE_ENV: "production",
-            DISCORD_TOKEN: process.env.DISCORD_TOKEN
-        },
+  apps: [
+    {
+      name: "keymash-discord",
+      script: "yarn",
+      args: "start",
+      env: {
+        NODE_ENV: "development",
+        DISCORD_TOKEN: process.env.DISCORD_TOKEN
       },
-    ],
-  };
+      env_production: {
+        NODE_ENV: "production",
+        DISCORD_TOKEN: process.env.DISCORD_TOKEN
+      }
+    }
+  ]
+};
