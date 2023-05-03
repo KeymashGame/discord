009615237f0ea49e42a6e59f7dfe419ecdfc8565
Cameron <CameronCT@users.noreply.github.com>
2023-05-02 22:51:02 -0600

    chore: remove intents since strings are no longer acceptable

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as Discord from "discord.js";
 import { Client } from "./structures/client";
 import { config } from "dotenv";
 import clientOptions from "./config/config.json";
@@ -12,6 +13,13 @@ if (process.env["TOKEN"] === undefined) {
   throw new Error("No token provided");
 }
 
-const client = new Client(clientOptions as Keymash.ClientOptions);
+const client = new Client({
+  ...clientOptions as Keymash.ClientOptions,
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.GuildMembers
+  ]
+});
 
 client.start(process.env["TOKEN"]).then(console.log);
