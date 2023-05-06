7c58c394d2c4eef58b2b76848980ff83d3031246
Evan <evanrayweigel@outlook.com>
2023-05-06 14:16:09 -0500

    convert to esm

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,15 @@
-module.exports = {
-  apps: [
-    {
-      name: "keymash-discord",
-      script: "yarn",
-      args: "start",
-      env: {
-        NODE_ENV: "development",
-        TOKEN: process.env["TOKEN"]
-      },
-      env_production: {
-        NODE_ENV: "production",
-        TOKEN: process.env["TOKEN"]
-      }
+export const apps = [
+  {
+    name: "keymash-discord",
+    script: "yarn",
+    args: "start",
+    env: {
+      NODE_ENV: "development",
+      TOKEN: process.env["TOKEN"]
+    },
+    env_production: {
+      NODE_ENV: "production",
+      TOKEN: process.env["TOKEN"]
     }
-  ]
-};
+  }
+];
