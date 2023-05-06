ebccee5d0af39aeb83d2f03caa8eebf9c8e0910d
Evan <evanrayweigel@outlook.com>
2023-05-06 15:21:47 -0500

    user command refactor and fix

--- src/functions/player.ts
+++ src/functions/player.ts
@@ -15,7 +15,7 @@ export async function getPlayerFromID<T extends GetPlayerType>(
   client: Client<true>,
   playerID: string,
   type: T
-): Promise<GetPlayerData<T>> {
+): Promise<GetPlayerData<T> | undefined> {
   const params = new URLSearchParams({
     playerId: playerID,
     worldId: "0"
@@ -27,14 +27,18 @@ export async function getPlayerFromID<T extends GetPlayerType>(
     )}`
   );
 
+  if (!response.ok) {
+    return;
+  }
+
   const json = await response.json();
 
   if (json === undefined) {
-    throw "No JSON returned";
+    return;
   }
 
   if (json.error !== undefined) {
-    throw json.error;
+    return;
   }
 
   return json;
@@ -44,7 +48,7 @@ export async function getPlayerFromUsername<T extends GetPlayerType>(
   client: Client<true>,
   username: string,
   type: T
-): Promise<GetPlayerData<T>> {
+): Promise<GetPlayerData<T> | undefined> {
   const params = new URLSearchParams({
     name: username,
     worldId: "0"
@@ -56,14 +60,18 @@ export async function getPlayerFromUsername<T extends GetPlayerType>(
     )}`
   );
 
+  if (!response.ok) {
+    return;
+  }
+
   const json = await response.json();
 
   if (json === undefined) {
-    throw "No JSON returned";
+    return;
   }
 
   if (json.error !== undefined) {
-    throw json.error;
+    return;
   }
 
   return json;
