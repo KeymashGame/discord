21b701e5a727b32539be77a351433bccb8e09e2f
Evan <evanrayweigel@outlook.com>
2023-05-06 14:17:17 -0500

    Revert "convert to esm"
    This reverts commit 7c58c394d2c4eef58b2b76848980ff83d3031246.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,17 @@
-export const apps = [
-  {
-    name: "keymash-discord",
-    script: "yarn",
-    args: "start",
-    env: {
-      NODE_ENV: "development",
-      TOKEN: process.env["TOKEN"]
-    },
-    env_production: {
-      NODE_ENV: "production",
-      TOKEN: process.env["TOKEN"]
+module.exports = {
+  apps: [
+    {
+      name: "keymash-discord",
+      script: "yarn",
+      args: "start",
+      env: {
+        NODE_ENV: "development",
+        TOKEN: process.env["TOKEN"]
+      },
+      env_production: {
+        NODE_ENV: "production",
+        TOKEN: process.env["TOKEN"]
+      }
     }
-  }
-];
+  ]
+};
