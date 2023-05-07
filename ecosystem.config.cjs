4bc9223668afd143e18f57039e92ffab5c92f1da
Cameron <CameronCT@users.noreply.github.com>
2023-05-07 00:17:34 -0600

    chore: attempt to use path set by setup-node

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
       name: "keymash-discord",
       script: "yarn",
       args: "start",
-      interpreter  : "/home/cameron/.nvm/versions/node/v18.16.0/bin/node",
+      interpreter  : "../../_tool/node/18.16.0/x64/bin/node",
       env: {
         NODE_ENV: "development",
         TOKEN: process.env["TOKEN"]
