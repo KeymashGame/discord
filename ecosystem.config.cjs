7a0cc603062658a0bdc1acaf9a1d6bfe73f08ec7
Cameron <CameronCT@users.noreply.github.com>
2023-05-07 00:16:11 -0600

    pls work action runner

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
       name: "keymash-discord",
       script: "yarn",
       args: "start",
-      interpreter  : "node@18.16.0",
+      interpreter  : "/home/cameron/.nvm/versions/node/v18.16.0/bin/node",
       env: {
         NODE_ENV: "development",
         TOKEN: process.env["TOKEN"]
