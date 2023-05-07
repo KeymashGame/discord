4aec23729b4951452e79cf04efb80ae2c01f14b2
Cameron <CameronCT@users.noreply.github.com>
2023-05-07 00:07:23 -0600

    added interpreter flag

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
       name: "keymash-discord",
       script: "yarn",
       args: "start",
+      interpreter  : "node@18.16.0",
       env: {
         NODE_ENV: "development",
         TOKEN: process.env["TOKEN"]
